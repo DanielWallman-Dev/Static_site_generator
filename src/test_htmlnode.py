29cd0eec5f1f8033afc0f5d954232bc057732516
Thailer423 <danielwallmandld@gmail.com>
2024-06-24 16:09:49 +0200

    Updated HTMLNode file with LeadNode child

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,11 +1,19 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
-    def test_props_to_html(self):
+    def test_to_html_props(self):
         node = HTMLNode( "","", "", {"href": "https://www.google.com", "target": "_blank"})
         self.assertEqual(node.props_to_html(), ' href="https://www.google.com" target="_blank"')
+    
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "My bridge")
+        self.assertEqual(node.to_html(), "<p>My bridge</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "My bridge")
+        self.assertEqual(node.to_html(), "My bridge")
 
 
 if __name__ == "__main__":
