daabe61feee33c9de7a521c6f39d2851cad14657
Thailer423 <danielwallmandld@gmail.com>
2024-06-27 16:00:47 +0200

    Added the functionality to determine each type of block that gest passed in and did some tests on it

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,14 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import (
+    markdown_to_blocks,
+    block_to_block_type,
+    block_type_paragraph,
+    block_type_heading,
+    block_type_code,
+    block_type_olist,
+    block_type_ulist,
+    block_type_quote,
+)
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -45,6 +54,22 @@ This is the same bridge and sign on a new line
                 "* This is a dome\n* with bridges",
             ],
         )
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "```\ndome and bridge\n```"
+        self.assertEqual(block_to_block_type(block), block_type_code)
+        block = "> dome\n> bridge"
+        self.assertEqual(block_to_block_type(block), block_type_quote)
+        block = "* domes\n* bridge"
+        self.assertEqual(block_to_block_type(block), block_type_ulist)
+        block = "- domes\n- bridge"
+        self.assertEqual(block_to_block_type(block), block_type_ulist)
+        block = "1. dome\n2. bridge"
+        self.assertEqual(block_to_block_type(block), block_type_olist)
+        block = "dome and bridges"
+        self.assertEqual(block_to_block_type(block), block_type_paragraph)
 
 
 if __name__ == "__main__":
