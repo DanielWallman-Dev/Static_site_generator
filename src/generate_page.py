35d164605f44b82f9e2ac40e01f2633a50014826
Thailer423 <danielwallmandld@gmail.com>
2024-07-05 14:46:05 +0200

    Updated the generate_page file to generate pages recursively, moved the old index.md to a new directory under the content directory and created a new index.md in the root of the content directory

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,5 +1,6 @@
 import os
 from markdown_blocks import markdown_to_html_node
+from pathlib import Path
 
 
 def extract_title(markdown):
@@ -30,3 +31,40 @@ def generate_page(from_path, template_path, dest_path):
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, 'w', encoding='utf-8') as dest_file:
         dest_file.write(final_content)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    # Ensuring the destination directory exist
+    os.makedirs(dest_dir_path, exist_ok=True)
+
+    for entry in os.listdir(dir_path_content):
+        # Full path to the current entry
+        full_path = os.path.join(dir_path_content, entry)
+
+        if os.path.isfile(full_path) and entry.endswith('.md'):
+            # Paths for reading and writing
+            from_path = full_path
+            to_path = os.path.join(dest_dir_path, entry.replace('.md', '.html'))
+            
+            
+            # Reading the files
+            with open(from_path, 'r', encoding='utf-8') as md_file:
+                markdown_content = md_file.read()
+
+            with open(template_path, 'r', encoding='utf-8') as tpl_file:
+                template_content = tpl_file.read()
+
+            # Converting markdown to HTML and extracting the title
+            HTML = markdown_to_html_node(markdown_content).to_html()
+            title = extract_title(markdown_content)
+
+            # Replacing placeholders in the template
+            final_content = template_content.replace("{{ Title }}", title).replace("{{ Content }}", HTML)
+
+            # Write the resulting HTML to the destination path
+            with open(to_path, 'w', encoding='utf-8') as html_file:
+                html_file.write(final_content)
+        
+        elif os.path.isdir(full_path):
+            # Recursive call to handle sub-directories
+            new_dest_dir = os.path.join(dest_dir_path, entry)
+            generate_pages_recursive(full_path, template_path, new_dest_dir)
